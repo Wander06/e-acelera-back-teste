e4e60a8c5da622bb76da032e747f5023fee39bc4
Wanderson Silva <wandersonsilva@Wandersons-MacBook-Pro.local>
2024-10-11 10:25:47 -0300

    colocando rotas dentro do arquivo do servidor

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT ?? 5002
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the homepage');
+    res.json('Welcome to the homepage');
 });
 
 app.get('/user', (req, res) => {
